faca35513236ab6d816c5d782c98314cc5ac1483
Shah Fahad <shahfaaad530@gmail.com>
2024-01-22 18:00:18 +0500

    auth pages

---
+++ app/(pages)/(auth)/register/page.tsx
@@ -0,0 +1,6 @@
+import RegisterComponent from "@/app/components/auth/RegisterComponent";
+import React from "react";
+
+export default function Register() {
+  return <RegisterComponent />;
+}
---
+++ app/(pages)/(auth)/signin/page.tsx
@@ -0,0 +1,6 @@
+import SigninComponent from "@/app/components/auth/SigninComponent";
+import React from "react";
+
+export default function page() {
+  return <SigninComponent />;
+}
