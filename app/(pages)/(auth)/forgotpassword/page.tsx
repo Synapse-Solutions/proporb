a1c6a637b32da1d1326dc0e56898bd0e9918afc5
iamhsntariq05 <hassantariq5544@gmail.com>
2024-05-16 22:04:31 +0500

    add is_owner checck

--- app/(pages)/(auth)/forgotpassword/page.tsx
+++ app/(pages)/(auth)/forgotpassword/page.tsx
@@ -13,8 +13,10 @@ export default function page() {
   const [Cpassword, setCpassword] = useState("");
 
   const onSendOtp = async () => {
+    let isOwner = localStorage.getItem("isOwner") || "owner";
     let data = {
       email: email,
+      is_owner :  isOwner !== "tenant"? true: false
     };
     console.log("🚀 ~ onSendOtp ~ data:", data);
     const response = await postApi("/v1/owner/send-email", data);
